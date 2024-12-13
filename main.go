953848939cc13d5e1ba5d162e54c8d4738253ee0
dastarruer <ayushpramanik399@gmail.com>
2024-12-13 18:55:07 -0400

    Every second, the current time is printed to the terminal

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
-)
+	"time"
+) 
 
 func main() {
-	fmt.Println("gday m8")
+	// Create a ticker; this is a channel that is given a value every 'd' 
+	ticker := time.NewTicker(time.Second)
+	done := make(chan bool)
+
+
+
+    go func() {
+        for {
+            select {
+            case <-done:
+                return
+            // Every time the ticker channel emits a value (aka one second passes)
+            case t := <-ticker.C:
+                fmt.Println("Tick at", t)
+            }
+        }
+    }()
+
+    time.Sleep(10 * time.Second)
+    ticker.Stop()
+    done <- true
+    fmt.Println("Ticker stopped")
+
 }
