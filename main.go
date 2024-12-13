1f182418e66f42923728216d91d17ef1bafbc546
dastarruer <ayushpramanik399@gmail.com>
2024-12-13 19:03:04 -0400

    After every tick, the seconds left is displayed

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	// Create a ticker; this is a channel that is given a value every 'd' 
 	ticker := time.NewTicker(time.Second)
 	done := make(chan bool)
+    secondsLeft := 10
 
     go func() {
         for {
@@ -18,11 +19,12 @@ func main() {
             case <-done:
                 return
             // Every time the ticker channel emits a value (aka one second passes)
-            case t := <-ticker.C:
+            case <-ticker.C:
+                secondsLeft -= 1
                 cmd := exec.Command("clear") // For Linux, macOS
                 cmd.Stdout = os.Stdout
                 cmd.Run()
-                fmt.Println("Tick at", t)
+                fmt.Println("Seconds left:", secondsLeft)
             }
         }
     }()
