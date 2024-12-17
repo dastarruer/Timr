c250e1dba24f0747878d4a1515429414bdb96c5d
dastarruer <ayushpramanik399@gmail.com>
2024-12-17 16:53:37 -0400

    App root is now flexbox rather than box

--- tui/tui.go
+++ tui/tui.go
@@ -5,11 +5,13 @@ import (
 )
 
 func HelloWorld() {
-	box := tview.NewBox().SetBorder(true).SetTitle("timr")
+	flex := tview.NewFlex()
+
+	// input := tview.NewInputField()
 
 	app := tview.NewApplication()
 
-	if err := app.SetRoot(box, true).Run(); err != nil {
+	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
 }
