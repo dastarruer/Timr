9af74dcf763f8c697958f05e62dbf30ec7adcdc9
dastarruer <ayushpramanik399@gmail.com>
2024-12-17 16:46:36 -0400

    Abstracted tview.NewApplication to app variable

--- tui/tui.go
+++ tui/tui.go
@@ -6,7 +6,10 @@ import (
 
 func HelloWorld() {
 	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	
+	app := tview.NewApplication()
+
+	if err := app.SetRoot(box, true).Run(); err != nil {
 		panic(err)
 	}
 }
