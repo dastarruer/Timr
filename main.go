693dc4f0c5c009b2706b3745b5b75d16a99567b3
dastarruer <ayushpramanik399@gmail.com>
2024-12-17 18:03:02 -0400

    user arguments are validated before the program starts

--- main.go
+++ main.go
@@ -3,25 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
 	"time"
 	"timr/timer"
+	"strconv"
 )
 
 func main() {
 	args := os.Args
+	validNumArgs := 2
 
-	pattern := `^([1-9]|1[0-9]|2[0-3]):([1-9]|[1-5][0-9]):([1-9]|[1-5][0-9])$`
-
-	re := regexp.MustCompile(pattern)
-
-	if !re.MatchString(args[1]) {
+	if len(args) != validNumArgs {
 		fmt.Println("usage: ./main.go [hours:minutes:seconds]")
 		os.Exit(1)
 	}
 
 	// Create a ticker; this is a channel that is given a value every time.Second
-	durationSeconds := 70
+	durationSeconds, err := strconv.Atoi(args[1])
+
+	// If the user provides an invalid time or mnumber of arguments
+	if err != nil {
+		fmt.Println("usage: ./main.go [hours:minutes:seconds]")
+		os.Exit(1)
+	}
+
 	timer := timer.CreateTimer(durationSeconds)
 
 	// Start the timer
