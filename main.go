027e5c8c4b706edb5294bdc00ac39da5344281df
dastarruer <ayushpramanik399@gmail.com>
2024-12-17 17:40:06 -0400

    added runtime argument support, regex to filter incorrect arguments

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"regexp"
 	"time"
 	"timr/timer"
 )
 
 func main() {
+	args := os.Args[1:]
+
+	pattern := `^([1-9]|1[0-9]|2[0-3]):([1-9]|[1-5][0-9]):([1-9]|[1-5][0-9])$`
+
+	re = regexp.MustCompile(pattern)
+
 	// Create a ticker; this is a channel that is given a value every time.Second
 	durationSeconds := 70
 	timer := timer.CreateTimer(durationSeconds)
